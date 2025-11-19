61e48e657460227b449b3f6822b47303836dcc90
johnnydev-tech <johnnyfreirejh@hotmail.com>
2025-11-19 19:12:36 -0300

    Initial Commit

---
+++ StateBinding/StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingApp.swift
+//  StateBinding
+//
+//  Created by Johnny Freire on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
